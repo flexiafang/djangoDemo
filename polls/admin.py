950bf69370172c90d2c56b9f1b8bcc6999258eee
flexiafang <1208689216@qq.com>
2021-03-04 17:55:29 +0800

    优化管理界面

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,30 @@ from django.contrib import admin
 
 from polls.models import Question, Choice
 
-# 向管理页面中加入投票应用
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    """
+    优化管理界面
+    """
+    # 修改默认的表单显示列表
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']})
+    ]
+    # Choice 对象要在 Question 后台页面编辑。默认提供 1 个足够的选项字段。
+    inlines = [ChoiceInline]
+    # 自定义后台管理列表
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    # 添加过滤器侧边栏
+    list_filter = ['pub_date']
+    # 增加搜索框
+    search_fields = ['question_text']
+
+
+# 向管理页面中注册模型
+admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -17,14 +17,19 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    # 优化管理页面展示效果
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
 
 class Choice(models.Model):
+    # 每个Choice对象都关联到一个Question对象
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     # 选项描述
     choice_text = models.CharField(max_length=200)
     # 当前票数
     votes = models.IntegerField(default=0)
-    # 每个Choice对象都关联到一个Question对象
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.choice_text
